41a4faeb69d337a5dbf3ed741331b8ee2189b8c8
Elson <elson@Elsons-MacBook-Pro.local>
2023-08-30 01:59:22 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+import { styles } from "./src/components/styles";
+import Ball from "./src/components/Ball";
 
+let timer: number;
 export default function App() {
+  const [gravity, setGravity] = useState(0.98);
+  const [upForce, setUpForce] = useState(0);
+  const [speed, setSpeed] = useState(0);
+  const [posY, setPosY] = useState(0);
+
+  useEffect(() => {
+    const applyGravity = () => {
+      let newUpForce = upForce - gravity;
+      newUpForce = newUpForce < 0 ? 0 : newUpForce;
+      setUpForce(newUpForce);
+
+      let newSpeed = speed + (gravity - newUpForce / 2);
+      setSpeed(newSpeed);
+
+      let newPosY = posY - newSpeed;
+
+      if (newPosY < 0) {
+        newPosY = 0;
+        setSpeed(0);
+      }
+      setPosY(newPosY);
+    };
+    clearTimeout(timer);
+    timer = setTimeout(applyGravity, 30);
+  }, [gravity, upForce, speed, posY]);
+
+  function handleForceButton() {
+    setUpForce(7);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.area}>
+        <Ball posY={posY} />
+      </View>
+      <View style={styles.control}>
+        <View>
+          <Text style={styles.controlText}>
+            UpForce:{` ${upForce.toFixed(2)}`}{" "}
+          </Text>
+          <Text style={styles.controlText}>
+            Velocity:{` ${speed.toFixed(2)}`}{" "}
+          </Text>
+          <Text style={styles.controlText}>PosY:{` ${posY.toFixed(2)}`}</Text>
+        </View>
+        <TouchableOpacity
+          style={styles.controlButton}
+          onPress={handleForceButton}
+        >
+          <Text style={styles.controlText}>Do Force</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function setState(arg0: number): [any, any] {
+  throw new Error("Function not implemented.");
+}
